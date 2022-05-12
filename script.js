3aa194aa15269d0bac6433e11b5b096c13713666
Dự NDQ <dundq@edoctor.vn>
2022-05-13 01:05:05 +0700

    .

--- script.js
+++ script.js
@@ -28,30 +28,40 @@ const assets = [
 ];
 (async () => {
   const opts = {
-    width: 310,
+    width: 200,
     height: 90,
   }
+  const qrcodeSize = opts.width/3 > opts.height ? opts.height : opts.width/3;
+  const logoSize = opts.width/3 > opts.height ? opts.height : opts.width/3
   const mapAsset = await Promise.all(
     assets.map(async (asset) => {
       return {
         ...asset,
         qrcode_image: await qrcode.toDataURL("abc", {
             margin: 0,
-            width: opts.width/3
+            width:qrcodeSize
         }),
       };
     })
   );
   const html = template({
     assets: mapAsset,
+    qrcode: {
+      size: qrcodeSize,
+    },
+    logo: {
+      size: logoSize / 2,
+      image: fs.readFileSync('fm-logo.png', 'base64'),
+    }
   });
+  console.log(html)
   fs.writeFileSync("./index.html", html);
   pdf
     .create(html, {
       format: "Letter",
       width: opts.width,
       height: opts.height,
-      quality: 100
+      quality: 100,
     })
     .toFile("./qrcode.pdf", function (err, res) {
       if (err) return console.log(err);
